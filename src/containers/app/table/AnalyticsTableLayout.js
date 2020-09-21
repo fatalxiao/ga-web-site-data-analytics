d5585c57a89f09d9aea6561c0d11bddcde544d8f
liangxiaojun <fatalxiao@163.com>
2020-09-21 16:18:52 +0800

    update

--- src/containers/app/table/AnalyticsTableLayout.js
+++ src/containers/app/table/AnalyticsTableLayout.js
@@ -3,7 +3,7 @@
  * @author liangxiaojun(liangxiaojun@derbysoft.com)
  */
 
-import React, {useRef, useState, useCallback, useEffect} from 'react';
+import React, {useRef, useMemo, useState, useCallback, useEffect} from 'react';
 import PropTypes from 'prop-types';
 
 // Components
@@ -15,28 +15,44 @@ import ResizeObserver from 'resize-observer-polyfill';
 // Styles
 import 'scss/containers/app/table/AnalyticsTableLayout.scss';
 
-let observer = null,
-    lastHeight = 0;
+let observer = null;
 
 const AnalyticsTableLayout = (props) => {
 
     const layout = useRef(),
 
+        /**
+         * 表头高度
+         * @type {number}
+         */
+        tableHeadHeight = useMemo(() => 48, []),
+
+        /**
+         * 测量出的高度
+         * @type {React.MutableRefObject<undefined>}
+         */
         [height, setHeight] = useState(0),
 
+        /**
+         * 处理高度的测量
+         * @type {Function}
+         */
         handleMeasure = useCallback(entries => {
 
             const target = entries[0].target,
                 {height} = target.getBoundingClientRect(),
-                fixedHeight = Math.floor(height);
+                fixedHeight = Math.floor(height) - tableHeadHeight;
 
-            if (lastHeight !== fixedHeight) {
-                lastHeight = fixedHeight;
+            if (height !== fixedHeight) {
                 setHeight(fixedHeight);
             }
 
         }),
 
+        /**
+         * 添加观测
+         * @type {Function}
+         */
         addObserver = useCallback(() => {
             if (layout?.current && !observer) {
                 observer = new ResizeObserver(handleMeasure);
@@ -44,6 +60,10 @@ const AnalyticsTableLayout = (props) => {
             }
         }),
 
+        /**
+         * 移除观测
+         * @type {Function}
+         */
         removeObserver = useCallback(() => {
             if (observer) {
                 observer.disconnect();
@@ -51,19 +71,19 @@ const AnalyticsTableLayout = (props) => {
             }
         });
 
+    /**
+     * 初始化时添加观测，销毁时移除观测
+     */
     useEffect(() => {
-
         addObserver();
-
         return () => removeObserver();
-
     }, []);
 
     return (
         <div ref={layout}
              className="analytics-table-layout">
             <Table {...props}
-                   scrollHeight={height - 48}/>
+                   scrollHeight={height}/>
         </div>
     );
 
