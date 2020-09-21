152dbe9c33df89c27c897be949a9dfefaef928fb
liangxiaojun <fatalxiao@163.com>
2020-09-21 13:45:31 +0800

    update build

--- build/run/server.js
+++ build/run/server.js
@@ -1,45 +1,14 @@
 const express = require('express'),
-    {createProxyMiddleware} = require('http-proxy-middleware'),
     history = require('connect-history-api-fallback'),
     open = require('open'),
     compression = require('compression'),
 
     config = require('../config.js'),
-    utils = require('../utils.js'),
 
     env = process.env.NODE_ENV,
     app = express(),
     port = config[env].port,
-    uri = 'http://localhost:' + port,
-
-    proxyTable = config[env].proxyTable;
-
-Object.keys(proxyTable).forEach(context => {
-
-    let options = proxyTable[context];
-
-    if (typeof options === 'string') {
-        options = {
-            target: options,
-            changeOrigin: true,
-            logLevel: 'error'
-        };
-    }
-
-    options.onProxyReq = (proxyReq, req, res) => {
-
-        const ip = utils.getClientIp(req);
-        ip && proxyReq.setHeader('ip', utils.ipParse(ip));
-
-        if (req.headers && !req.headers.token && req.query && req.query.token) {
-            proxyReq.setHeader('token', req.query.token);
-        }
-
-    };
-
-    app.use(createProxyMiddleware(options.filter || context, options));
-
-});
+    uri = 'http://localhost:' + port;
 
 app.use(compression())
    .use(history())
@@ -50,9 +19,6 @@ app.use(compression())
            );
        }
    }))
-   .get('/derby_forever_health/status.ci', (req, res) => {
-       res.sendStatus(200);
-   })
    .listen(port, err => {
 
        if (err) {
