81582eecd391c201cbf356d3ce4605b3a0131260
liangxiaojun <fatalxiao@163.com>
2020-11-27 16:35:45 +0800

    update getDigitLength test

---
+++ test/unit/Util.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Vendors
+import {getDigitLength} from 'vendors/Util';
+
+/**
+ * getDigitLength 测试
+ */
+describe('getDigitLength test', () => {
+
+    test('getDigitLength 100', () => {
+        expect(getDigitLength(100)).toBe(0);
+    });
+
+    test('getDigitLength 0', () => {
+        expect(getDigitLength(0)).toBe(0);
+    });
+
+    test('getDigitLength 0.1', () => {
+        expect(getDigitLength(0.1)).toBe(1);
+    });
+
+    test('getDigitLength "0.1"', () => {
+        expect(getDigitLength('0.1')).toBe(1);
+    });
+
+    test('getDigitLength 0.000001', () => {
+        expect(getDigitLength(0.000001)).toBe(6);
+    });
+
+    test('getDigitLength 100.000001', () => {
+        expect(getDigitLength(100.000001)).toBe(6);
+    });
+
+    test('getDigitLength "100.000001"', () => {
+        expect(getDigitLength('100.000001')).toBe(6);
+    });
+
+});
