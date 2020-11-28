a4af6f7f1de1947c95df341f4ee69eee180f1438
Liangxiaojun <fatalxiao@163.com>
2020-11-28 19:00:39 +0800

    update TimeColumnUtil.test

--- test/unit/TimeColumnUtil.test.js
+++ test/unit/TimeColumnUtil.test.js
@@ -21,25 +21,40 @@ import {
  */
 describe('isValidTime test', () => {
 
-    test('isValidTime "00:00:00"', () =>
-        expect(isValidTime('00:00:00')).toBe(true)
-    );
+    {
+        const value = '00:00:00';
+        test(`isValidTime ${value}`, () =>
+            expect(isValidTime(value)).toBe(true)
+        );
+    }
 
-    test('isValidTime "10:10:10"', () =>
-        expect(isValidTime('10:10:10')).toBe(true)
-    );
+    {
+        const value = '10:10:10';
+        test(`isValidTime ${value}`, () =>
+            expect(isValidTime(value)).toBe(true)
+        );
+    }
 
-    test('isValidTime "10:10"', () =>
-        expect(isValidTime('10:10')).toBe(false)
-    );
+    {
+        const value = '10:10';
+        test(`isValidTime ${value}`, () =>
+            expect(isValidTime(value)).toBe(false)
+        );
+    }
 
-    test('isValidTime "10"', () =>
-        expect(isValidTime('10')).toBe(false)
-    );
+    {
+        const value = '10';
+        test(`isValidTime ${value}`, () =>
+            expect(isValidTime(value)).toBe(false)
+        );
+    }
 
-    test('isValidTime "1000:00:10"', () =>
-        expect(isValidTime('1000:00:10')).toBe(true)
-    );
+    {
+        const value = '1000:00:10';
+        test(`isValidTime ${value}`, () =>
+            expect(isValidTime(value)).toBe(true)
+        );
+    }
 
 });
 
@@ -48,56 +63,201 @@ describe('isValidTime test', () => {
  */
 describe('countTime test', () => {
 
-    test('countTime "00:00:00"', () =>
-        expect(countTime('00:00:00')).toBe(0)
+    {
+        const value = '00:00:00';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(0)
+        );
+    }
+
+    {
+        const value = '00:00:01';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(1)
+        );
+    }
+
+    {
+        const value = '00:00:20';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(20)
+        );
+    }
+
+    {
+        const value = '00:01:00';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(60)
+        );
+    }
+
+    {
+        const value = '00:20:20';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(20 * 60 + 20)
+        );
+    }
+
+    {
+        const value = '01:20:20';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(60 * 60 + 20 * 60 + 20)
+        );
+    }
+
+    {
+        const value = '20:20:20';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(20 * 60 * 60 + 20 * 60 + 20)
+        );
+    }
+
+    {
+        const value = '20:20';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(0)
+        );
+    }
+
+    {
+        const value = '20';
+        test(`countTime ${value}`, () =>
+            expect(countTime(value)).toBe(0)
+        );
+    }
+
+});
+
+/**
+ * fillZero 测试
+ */
+describe('fillZero test', () => {
+
+    {
+        const value = '0';
+        test(`fillZero ${value}`, () =>
+            expect(fillZero(value)).toBe('00')
+        );
+    }
+
+    {
+        const value = '6';
+        test(`fillZero ${value}`, () =>
+            expect(fillZero(value)).toBe('06')
+        );
+    }
+
+    {
+        const value = '10';
+        test(`fillZero ${value}`, () =>
+            expect(fillZero(value)).toBe('10')
+        );
+    }
+
+    {
+        const value = '100';
+        test(`fillZero ${value}`, () =>
+            expect(fillZero(value)).toBe('100')
+        );
+    }
+
+    {
+        const value = 0;
+        test(`fillZero ${value}`, () =>
+            expect(fillZero(value)).toBe('00')
+        );
+    }
+
+    {
+        const value = -1;
+        test(`fillZero ${value}`, () =>
+            expect(fillZero(value)).toBe('-1')
+        );
+    }
+
+});
+
+/**
+ * formatTime 测试
+ */
+describe('formatTime test', () => {
+
+    test('formatTime 0', () =>
+        expect(formatTime(0)).toBe('00:00:00')
     );
 
-    test('countTime "00:00:01"', () =>
-        expect(countTime('00:00:01')).toBe(1)
+    test('formatTime 1', () =>
+        expect(formatTime(1)).toBe('00:00:01')
     );
 
-    test('countTime "00:00:20"', () =>
-        expect(countTime('00:00:20')).toBe(20)
+    test('formatTime 20', () =>
+        expect(formatTime(20)).toBe('00:00:20')
     );
 
-    test('countTime "00:01:00"', () =>
-        expect(countTime('00:01:00')).toBe(60)
+    test('formatTime 59', () =>
+        expect(formatTime(59)).toBe('00:00:59')
     );
 
-    test('countTime "00:20:20"', () =>
-        expect(countTime('00:20:20')).toBe(20 * 60 + 20)
+    test('formatTime 60', () =>
+        expect(formatTime(60)).toBe('00:01:00')
     );
 
-    test('countTime "01:20:20"', () =>
-        expect(countTime('01:20:20')).toBe(60 * 60 + 20 * 60 + 20)
-    );
+    {
+        const value = 20 * 60;
+        test(`formatTime ${value}`, () =>
+            expect(formatTime(value)).toBe('00:20:00')
+        );
+    }
 
-    test('countTime "20:20"', () =>
-        expect(countTime('20:20')).toBe(0)
-    );
+    {
+        const value = 20 * 60 + 20;
+        test(`formatTime ${value}`, () =>
+            expect(formatTime(value)).toBe('00:20:20')
+        );
+    }
 
-    test('countTime "20"', () =>
-        expect(countTime('20')).toBe(0)
+    {
+        const value = 60 * 60;
+        test(`formatTime ${value}`, () =>
+            expect(formatTime(value)).toBe('01:00:00')
+        );
+    }
+
+    {
+        const value = 60 * 60 + 20 * 60 + 20;
+        test(`formatTime ${value}`, () =>
+            expect(formatTime(value)).toBe('01:20:20')
+        );
+    }
+
+    {
+        const value = 20 * 60 * 60 + 20 * 60 + 20;
+        test(`formatTime ${value}`, () =>
+            expect(formatTime(value)).toBe('20:20:20')
+        );
+    }
+
+    test('formatTime -1', () =>
+        expect(formatTime(-1)).toBe('00:00:00')
     );
 
 });
 
-describe('fillZero test', () => {
+/**
+ * getAverageTime 测试
+ */
+describe('getAverageTime test', () => {
 
-    test('fillZero "0"', () =>
-        expect(fillZero('0')).toBe('00')
+    test('getAverageTime ["00:00:00"]', () =>
+        expect(getAverageTime(['00:00:00'])).toBe('00:00:00')
     );
 
-    test('fillZero "6"', () =>
-        expect(fillZero('6')).toBe('06')
+    test('getAverageTime ["00:00:20"]', () =>
+        expect(getAverageTime(['00:00:20'])).toBe('00:00:20')
     );
 
-    test('fillZero "10"', () =>
-        expect(fillZero('10')).toBe('10')
-    );
-
-    test('fillZero "100"', () =>
-        expect(fillZero('100')).toBe('100')
+    test('getAverageTime ["00:00:00", "00:00:20"]', () =>
+        expect(getAverageTime(['00:00:00', '00:00:20'])).toBe('00:00:10')
     );
 
 });
