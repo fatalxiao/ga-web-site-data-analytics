d29ee1398870696302c95ab883acc81bc5977bd7
Liangxiaojun <fatalxiao@163.com>
2020-11-28 19:05:38 +0800

    update Util.test

--- test/unit/Util.test.js
+++ test/unit/Util.test.js
@@ -15,32 +15,67 @@ import {getDigitLength} from 'vendors/Util';
  */
 describe('getDigitLength test', () => {
 
-    test('getDigitLength 100', () =>
-        expect(getDigitLength(100)).toBe(0)
-    );
+    {
+        const value = 0;
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(0)
+        );
+    }
 
-    test('getDigitLength 0', () =>
-        expect(getDigitLength(0)).toBe(0)
-    );
+    {
+        const value = 100;
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(0)
+        );
+    }
 
-    test('getDigitLength 0.1', () =>
-        expect(getDigitLength(0.1)).toBe(1)
-    );
+    {
+        const value = 0.1;
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(1)
+        );
+    }
 
-    test('getDigitLength "0.1"', () =>
-        expect(getDigitLength('0.1')).toBe(1)
-    );
+    {
+        const value = '0.1';
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(1)
+        );
+    }
 
-    test('getDigitLength 0.000001', () =>
-        expect(getDigitLength(0.000001)).toBe(6)
-    );
+    {
+        const value = 0.000001;
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(6)
+        );
+    }
 
-    test('getDigitLength 100.000001', () =>
-        expect(getDigitLength(100.000001)).toBe(6)
-    );
+    {
+        const value = 100.000001;
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(6)
+        );
+    }
 
-    test('getDigitLength "100.000001"', () =>
-        expect(getDigitLength('100.000001')).toBe(6)
-    );
+    {
+        const value = '100.000001';
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(6)
+        );
+    }
+
+    {
+        const value = -1;
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(0)
+        );
+    }
+
+    {
+        const value = -0.1;
+        test(`getDigitLength ${value}`, () =>
+            expect(getDigitLength(value)).toBe(1)
+        );
+    }
 
 });
