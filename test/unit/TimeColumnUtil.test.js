fff4c3841fd9fae991f8c46b46e1cca468539585
Liangxiaojun <fatalxiao@163.com>
2020-11-28 18:20:27 +0800

    update TimeColumnUtil.test.js

--- test/unit/TimeColumnUtil.test.js
+++ test/unit/TimeColumnUtil.test.js
@@ -5,6 +5,7 @@
 
 'use strict';
 
+
 // Vendors
 import {
     isValidTime,
@@ -14,27 +15,70 @@ import {
     getAverageTime
 } from 'vendors/TimeColumnUtil';
 
+/* eslint-disable no-undef */
+
 /**
  * isValidTime 测试
  */
 describe('isValidTime test', () => {
-    test('isValidTime "00:00:00"', () => expect(isValidTime('00:00:00')).toBe(true));
-    test('isValidTime "10:10:10"', () => expect(isValidTime('10:10:10')).toBe(true));
-    test('isValidTime "10:10"', () => expect(isValidTime('10:10')).toBe(false));
-    test('isValidTime "10"', () => expect(isValidTime('10')).toBe(false));
-    test('isValidTime "1000:00:10"', () => expect(isValidTime('1000:00:10')).toBe(true));
+
+    test('isValidTime "00:00:00"', () =>
+        expect(isValidTime('00:00:00')).toBe(true)
+    );
+
+    test('isValidTime "10:10:10"', () =>
+        expect(isValidTime('10:10:10')).toBe(true)
+    );
+
+    test('isValidTime "10:10"', () =>
+        expect(isValidTime('10:10')).toBe(false)
+    );
+
+    test('isValidTime "10"', () =>
+        expect(isValidTime('10')).toBe(false)
+    );
+
+    test('isValidTime "1000:00:10"', () =>
+        expect(isValidTime('1000:00:10')).toBe(true)
+    );
+
 });
 
 /**
  * countTime 测试
  */
 describe('countTime test', () => {
-    test('countTime "00:00:00"', () => expect(countTime('00:00:00')).toBe(0));
-    test('countTime "00:00:01"', () => expect(countTime('00:00:01')).toBe(1));
-    test('countTime "00:00:20"', () => expect(countTime('00:00:20')).toBe(20));
-    test('countTime "00:01:00"', () => expect(countTime('00:01:00')).toBe(60));
-    test('countTime "00:20:20"', () => expect(countTime('00:20:20')).toBe(1220));
-    test('countTime "01:20:20"', () => expect(countTime('01:20:20')).toBe(4820));
-    test('countTime "20:20"', () => expect(countTime('20:20')).toBe(0));
-    test('countTime "20"', () => expect(countTime('20')).toBe(0));
+
+    test('countTime "00:00:00"', () =>
+        expect(countTime('00:00:00')).toBe(0)
+    );
+
+    test('countTime "00:00:01"', () =>
+        expect(countTime('00:00:01')).toBe(1)
+    );
+
+    test('countTime "00:00:20"', () =>
+        expect(countTime('00:00:20')).toBe(20)
+    );
+
+    test('countTime "00:01:00"', () =>
+        expect(countTime('00:01:00')).toBe(60)
+    );
+
+    test('countTime "00:20:20"', () =>
+        expect(countTime('00:20:20')).toBe(20 * 60 + 20)
+    );
+
+    test('countTime "01:20:20"', () =>
+        expect(countTime('01:20:20')).toBe(60 * 60 + 20 * 60 + 20)
+    );
+
+    test('countTime "20:20"', () =>
+        expect(countTime('20:20')).toBe(0)
+    );
+
+    test('countTime "20"', () =>
+        expect(countTime('20')).toBe(0)
+    );
+
 });
