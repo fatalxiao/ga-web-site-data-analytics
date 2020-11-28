3bc33e77a05f12c923e89426d7d32fd5bd1b69f1
Liangxiaojun <fatalxiao@163.com>
2020-11-28 18:15:49 +0800

    update Util.test.js

--- test/unit/Util.test.js
+++ test/unit/Util.test.js
@@ -13,32 +13,32 @@ import {getDigitLength} from 'vendors/Util';
  */
 describe('getDigitLength test', () => {
 
-    test('getDigitLength 100', () => {
-        expect(getDigitLength(100)).toBe(0);
-    });
+    test('getDigitLength 100', () =>
+        expect(getDigitLength(100)).toBe(0)
+    );
 
-    test('getDigitLength 0', () => {
-        expect(getDigitLength(0)).toBe(0);
-    });
+    test('getDigitLength 0', () =>
+        expect(getDigitLength(0)).toBe(0)
+    );
 
-    test('getDigitLength 0.1', () => {
-        expect(getDigitLength(0.1)).toBe(1);
-    });
+    test('getDigitLength 0.1', () =>
+        expect(getDigitLength(0.1)).toBe(1)
+    );
 
-    test('getDigitLength "0.1"', () => {
-        expect(getDigitLength('0.1')).toBe(1);
-    });
+    test('getDigitLength "0.1"', () =>
+        expect(getDigitLength('0.1')).toBe(1)
+    );
 
-    test('getDigitLength 0.000001', () => {
-        expect(getDigitLength(0.000001)).toBe(6);
-    });
+    test('getDigitLength 0.000001', () =>
+        expect(getDigitLength(0.000001)).toBe(6)
+    );
 
-    test('getDigitLength 100.000001', () => {
-        expect(getDigitLength(100.000001)).toBe(6);
-    });
+    test('getDigitLength 100.000001', () =>
+        expect(getDigitLength(100.000001)).toBe(6)
+    );
 
-    test('getDigitLength "100.000001"', () => {
-        expect(getDigitLength('100.000001')).toBe(6);
-    });
+    test('getDigitLength "100.000001"', () =>
+        expect(getDigitLength('100.000001')).toBe(6)
+    );
 
 });
