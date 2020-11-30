3dccb3ccf3393d1fb1ac283212a4c0b6283e0226
liangxiaojun <fatalxiao@163.com>
2020-11-30 09:45:32 +0800

    update PercentColumnUtil.test

--- test/unit/PercentColumnUtil.test.js
+++ test/unit/PercentColumnUtil.test.js
@@ -83,3 +83,115 @@ describe('isValidPercent test', () => {
     }
 
 });
+
+/**
+ * formatPercent 测试
+ */
+describe('formatPercent test', () => {
+
+    {
+        const value = 1;
+        test(`formatPercent ${value}`, () =>
+            expect(formatPercent(value)).toBe('100.00%')
+        );
+    }
+
+    {
+        const value = 0.1;
+        test(`formatPercent ${value}`, () =>
+            expect(formatPercent(value)).toBe('10.00%')
+        );
+    }
+
+    {
+        const value = 0.01;
+        test(`formatPercent ${value}`, () =>
+            expect(formatPercent(value)).toBe('1.00%')
+        );
+    }
+
+    {
+        const value = 0.001;
+        test(`formatPercent ${value}`, () =>
+            expect(formatPercent(value)).toBe('0.10%')
+        );
+    }
+
+    {
+        const value = 0.0001;
+        test(`formatPercent ${value}`, () =>
+            expect(formatPercent(value)).toBe('0.01%')
+        );
+    }
+
+    {
+        const value = 0.00001;
+        test(`formatPercent ${value}`, () =>
+            expect(formatPercent(value)).toBe('0.001%')
+        );
+    }
+
+    {
+        const value = null;
+        test(`formatPercent ${value}`, () =>
+            expect(formatPercent(value)).toBe('0.00%')
+        );
+    }
+
+    {
+        const value = '1';
+        test(`formatPercent ${value}`, () =>
+            expect(formatPercent(value)).toBe('100.00%')
+        );
+    }
+
+});
+
+/**
+ * getAveragePercent 测试
+ */
+describe('getAveragePercent test', () => {
+
+    {
+        const value = ['1%'];
+        test(`getAveragePercent [${value}]`, () =>
+            expect(getAveragePercent(value)).toBe('1.00%')
+        );
+    }
+
+    {
+        const value = ['1%', '1%'];
+        test(`getAveragePercent [${value}]`, () =>
+            expect(getAveragePercent(value)).toBe('1.00%')
+        );
+    }
+
+    {
+        const value = ['0%', '1%'];
+        test(`getAveragePercent [${value}]`, () =>
+            expect(getAveragePercent(value)).toBe('0.50%')
+        );
+    }
+
+    {
+        const value = ['0%', '1%', '2%'];
+        test(`getAveragePercent [${value}]`, () =>
+            expect(getAveragePercent(value)).toBe('1.00%')
+        );
+    }
+
+    {
+        const value = ['0', '1%'];
+        test(`getAveragePercent [${value}]`, () =>
+            expect(getAveragePercent(value)).toBe('1.00%')
+        );
+    }
+
+    {
+        const value = ['%', '1%'];
+        test(`getAveragePercent [${value}]`, () =>
+            expect(getAveragePercent(value)).toBe('1.00%')
+        );
+    }
+
+});
