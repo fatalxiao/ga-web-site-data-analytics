a217550fe858cd47844290e213534e72fbe8f573
liangxiaojun <fatalxiao@163.com>
2020-12-03 11:51:03 +0800

    update DropFile

--- src/containers/selectFile/DropFile.js
+++ src/containers/selectFile/DropFile.js
@@ -3,7 +3,7 @@
  * @author liangxiaojun(liangxiaojun@derbysoft.com)
  */
 
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import PropTypes from 'prop-types';
 
 // Icons
@@ -11,6 +11,7 @@ import UploadIcon from 'components/UploadIcon';
 
 // Vendors
 import classNames from 'classnames';
+import Event from 'alcedo-ui/_vendors/Event';
 
 // Styles
 import 'scss/containers/selectFile/DropFile.scss';
@@ -18,8 +19,29 @@ import 'scss/containers/selectFile/DropFile.scss';
 const DropFile = ({
     children, className
 }) => {
+
+    const [dragging, setDragging] = useState(false),
+
+        [draggingOver, setDraggingOver] = useState(false),
+
+        handleWindowDragEnter = useCallback(() => setDragging(true), []),
+        handleWindowDragLeave = useCallback(() => setDragging(false), []);
+
+    useEffect(() => {
+
+        Event.addEvent(document, 'dragenter', handleWindowDragEnter);
+        Event.addEvent(document, 'dragleave', handleWindowDragLeave);
+
+        return () => {
+            Event.removeEvent(document, 'dragenter', handleWindowDragEnter);
+            Event.removeEvent(document, 'dragleave', handleWindowDragLeave);
+        };
+
+    }, []);
+
     return (
         <div className={classNames('drop-file', {
+            dragging,
             [className]: className
         })}>
 
@@ -34,6 +56,7 @@ const DropFile = ({
 
         </div>
     );
+
 };
 
 DropFile.propTypes = {
