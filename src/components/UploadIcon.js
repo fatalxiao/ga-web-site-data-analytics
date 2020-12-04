10773d3317634b56681852f11e106efc6f6bc7ec
liangxiaojun <fatalxiao@163.com>
2020-12-04 10:50:08 +0800

    update DropZone

--- src/components/UploadIcon.js
+++ src/components/UploadIcon.js
@@ -4,21 +4,32 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // Icons
 import CloudIcon from './CloudIcon';
 import ArrowUpIcon from './ArrowUpIcon';
 
+// Vendors
+import classNames from 'classnames';
+
 // Styles
 import 'scss/components/UploadIcon.scss';
 
-const UploadIcon = () => (
-    <div className="upload-icon">
+const UploadIcon = ({className, ...restProps}) => (
+    <div {...restProps}
+         className={classNames('upload-icon', {
+             [className]: className
+         })}>
         <CloudIcon/>
         <div className="arrow-up-icon-wrapper">
             <ArrowUpIcon/>
         </div>
     </div>
 );
+
+UploadIcon.propTypes = {
+    className: PropTypes.string
+};
 
 export default UploadIcon;
