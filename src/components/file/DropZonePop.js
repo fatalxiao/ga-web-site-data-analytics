bd1d7b1966cd1f3f59cf1a92b0f7c28d5eb8010a
liangxiaojun <fatalxiao@163.com>
2020-12-07 16:42:20 +0800

    DropZonePop bug fixed

--- src/components/file/DropZonePop.js
+++ src/components/file/DropZonePop.js
@@ -3,7 +3,7 @@
  * @author liangxiaojun(liangxiaojun@derbysoft.com)
  */
 
-import React from 'react';
+import React, {useMemo, useCallback} from 'react';
 
 // Components
 import PositionPop from 'alcedo-ui/_PositionPop';
@@ -11,6 +11,11 @@ import DropZone from './DropZone';
 
 // Hooks
 import useDraggingOver from 'hooks/useDraggingOver';
+
+// Vendors
+import addClass from 'dom-helpers/addClass';
+import removeClass from 'dom-helpers/removeClass';
+import hasClass from 'dom-helpers/hasClass';
 
 // Styles
 import './DropZonePop.scss';
@@ -22,14 +27,44 @@ const DropZonePop = () => {
         /**
          * 是否在 document 上 dragging over
          */
-        documentDraggingOver = useDraggingOver();
+        documentDraggingOver = useDraggingOver(),
+
+        /**
+         * body 上 blur 的 class name
+         * @type {string}
+         */
+        blurClassName = useMemo(() => 'drop-zone-pop-blur', []),
+
+        /**
+         * 为 body 添加 class
+         * @type {Function}
+         */
+        handleRender = useCallback(() => {
+            const body = document.querySelector('body');
+            if (!hasClass(body, blurClassName)) {
+                addClass(body, blurClassName);
+            }
+        }, []),
+
+        /**
+         * 移除 body 上的 class
+         * @type {Function}
+         */
+        handleDestroy = useCallback(() => {
+            const body = document.querySelector('body');
+            if (hasClass(body, blurClassName)) {
+                removeClass(body, blurClassName);
+            }
+        }, []);
 
     return (
         <PositionPop className="drop-zone-pop"
                      modalClassName="drop-zone-pop-modal"
                      visible={documentDraggingOver}
                      position={PositionPop.Position.CENTER}
-                     showModal={true}>
+                     showModal={true}
+                     onRender={handleRender}
+                     onDestroy={handleDestroy}>
             <div className="drop-zone-pop-content">
                 <DropZone>
                     <div className="drop-zone-pop-desc">Drag and drop your file here</div>
