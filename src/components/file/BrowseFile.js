5c3f6e14c7cd581dddaf298882975afcdf079d8b
liangxiaojun <fatalxiao@163.com>
2020-12-10 13:55:53 +0800

    BrowseFile bug fixed

--- src/components/file/BrowseFile.js
+++ src/components/file/BrowseFile.js
@@ -3,7 +3,7 @@
  * @author liangxiaojun(liangxiaojun@derbysoft.com)
  */
 
-import React, {useCallback, forwardRef} from 'react';
+import React, {useState, useCallback, forwardRef} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
@@ -18,13 +18,19 @@ const BrowseFile = forwardRef(({
 
     const
 
+        [inputKey, setInputKey] = useState(1),
+
         /**
          * 处理文件变更
          */
-        handleFileChange = useCallback(e => updateFile?.(e?.target?.files?.[0]), [updateFile]);
+        handleFileChange = useCallback(e => {
+            updateFile?.(e?.target?.files?.[0]);
+            setInputKey(inputKey + 1);
+        }, [updateFile]);
 
     return (
-        <input ref={ref}
+        <input key={inputKey}
+               ref={ref}
                className="hidden-input"
                type="file"
                accept="text/csv"
