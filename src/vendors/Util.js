6c4b16d76b6d1bfa6faabc3c8e75ed45c4606010
liangxiaojun <fatalxiao@163.com>
2020-12-17 13:25:02 +0800

    update Util

--- src/vendors/Util.js
+++ src/vendors/Util.js
@@ -14,6 +14,31 @@ export function getDigitLength(value) {
     return len > 0 ? len : 0;
 }
 
+/**
+ * 添加数值的千分位
+ * @param value 数值
+ * @param removeDecimal 是否舍去小数部分，默认：false
+ * @returns {any}
+ */
+export function formatMilli(value, removeDecimal = false) {
+
+    if (removeDecimal) {
+        value = Math.round(value);
+    }
+
+    if (!value) {
+        return value;
+    }
+
+    const isNegative = +value < 0;
+    value = /^-?(.*)$/.exec(value)[1];
+
+    return (isNegative ? '-' : '') + value.toString().replace(/(^|\s)\d+/g,
+        m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','));
+
+}
+
 export default {
-    getDigitLength
+    getDigitLength,
+    formatMilli
 };
