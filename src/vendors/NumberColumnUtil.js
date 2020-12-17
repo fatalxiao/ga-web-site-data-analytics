98f28d1298563d30b8cd04ba1cd9d3eefcfea163
liangxiaojun <fatalxiao@163.com>
2020-12-17 13:24:55 +0800

    update NumberColumnUtil

---
+++ src/vendors/NumberColumnUtil.js
@@ -0,0 +1,58 @@
+/**
+ * @file NumberColumnUtil.js
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+// Vendors
+import {formatMilli} from './Util';
+
+/**
+ * 数值格式的正则
+ * @type {RegExp}
+ */
+const REG = /^[\d,]+(.\d+)?$/;
+
+/**
+ * 是否是合法的数值
+ * @param value
+ * @returns {boolean}
+ */
+export function isValidNumber(value) {
+    return REG.test(value);
+}
+
+/**
+ * 解析数值
+ * @param value
+ * @returns {number}
+ */
+export function parseNumber(value) {
+
+    if (!isValidNumber(value)) {
+        return 0;
+    }
+
+    return +(value.replace(',', '')) || 0;
+
+}
+
+/**
+ * 格式化数值
+ * @param value
+ * @returns {number}
+ */
+export function formatNumber(value) {
+
+    if (isNaN(value)) {
+        return '0';
+    }
+
+    return formatMilli(value);
+
+}
+
+export default {
+    isValidNumber,
+    parseNumber,
+    formatNumber
+};
